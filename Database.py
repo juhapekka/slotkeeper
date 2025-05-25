b384a6eb29d7a42c7676d07fd7b736d441485e0d
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2025-05-25 22:55:00 +0300

    Database.py: start adding device handling stuff
    Here added add_device() and get_all_devices()
    
    Signed-off-by: Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>

--- Database.py
+++ Database.py
@@ -25,3 +25,26 @@ class Database:
         user = conn.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
         conn.close()
         return user
+
+    def add_device(self, name, description, created_by):
+        conn = self._connect()
+        try:
+            conn.execute(
+                "INSERT INTO devices (name, description, created_by) VALUES (?, ?, ?)",
+                (name, description, created_by)
+            )
+            conn.commit()
+            return True
+        except Exception as e:
+            print("Error adding device:", e)
+            return False
+        finally:
+            conn.close()
+
+    def get_all_devices(self):
+        conn = self._connect()
+        try:
+            devices = conn.execute("SELECT * FROM devices").fetchall()
+            return devices
+        finally:
+            conn.close()
