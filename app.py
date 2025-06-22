fd0eee0f86f67fccaca19338643c8c7d09a6029e
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2025-06-23 00:09:10 +0300

    app.py: add pie charts on user page
    list some statistics on user page in form of pie charts,
    left chart show most reserved machines by reservation number,
    right chart show most reserved machines by reservation time.
    
    Signed-off-by: Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>

--- app.py
+++ app.py
@@ -354,6 +354,70 @@ def view_device(device_id):
         csrf_token=csrf_token
     )
 
+def generate_pie_chart_segments(device_list, colors, value_key='value', label_key='name',
+                                preform_key=None):
+    '''generate data to be shown in pie chart. top 3 machines + rest.'''
+    pie_data = []
+    gradient_parts = []
+    total_value = sum(item.get(value_key, 0) for item in device_list)
+    cur_angle = 0
+
+    if total_value > 0:
+        sorted_data = sorted(device_list, key=lambda x: x.get(value_key, 0), reverse=True)
+        top_n = 3
+        num_items_total = len(sorted_data)
+
+        pie_items = []
+        if num_items_total <= top_n:
+            pie_items.extend(sorted_data)
+        else:
+            pie_items.extend(sorted_data[:top_n])
+            others_value = sum(d.get(value_key, 0) for d in sorted_data[top_n:])
+            if others_value > 0:
+                other_item_data = {label_key: 'Other Devices', value_key: others_value}
+                if preform_key == 'formatted_duration':
+                    other_item_data[preform_key] = format_duration_to_string(others_value)
+                pie_items.append(other_item_data)
+
+        for i, input_list in enumerate(pie_items):
+            item_nvalue = input_list.get(value_key, 0)
+            percentage = 0
+            if total_value > 0:
+                percentage = (item_nvalue / total_value) * 100
+
+            # dict to template
+            item_dict = {
+                'name': input_list.get(label_key, 'Unknown'),
+                'value_numeric': item_nvalue,
+                'percentage': round(percentage, 1),
+                'color': colors[i % len(colors)]
+            }
+
+            if preform_key:
+                if preform_key in input_list:
+                    item_dict[preform_key] = input_list[preform_key]
+                else:
+                    item_dict[preform_key] = str(item_nvalue)
+
+            pie_data.append(item_dict)
+
+            start_percentage = cur_angle
+            is_last_segment = i == len(pie_items) - 1
+            if is_last_segment and cur_angle < 100:
+                cur_angle = 100.0
+            elif total_value > 0 :
+                cur_angle += percentage
+            cur_angle = min(max(cur_angle, 0), 100.0)
+            start_percentage = min(max(start_percentage,0), 100.0)
+            gradient_parts.append(
+                f'{colors[i % len(colors)]} {round(start_percentage, 2)}% {round(cur_angle, 2)}%'
+            )
+
+    conic_gradient_style = ''
+    if gradient_parts:
+        conic_gradient_style = f'background: conic-gradient({', '.join(gradient_parts)});'
+    return pie_data, conic_gradient_style, (total_value > 0)
+
 @app.route('/user')
 @login_required_with_csrf
 def user_page():
@@ -362,11 +426,52 @@ def user_page():
     reservations = db.get_active_reservations_by_user(username)
     devices = db.get_devices_created_by_user(username)
     last_reservations = db.get_last_reservations_by_user(username)
+    user = db.get_user_by_username(username)
+
+    if not user:
+        session.pop('username', None)
+        session.pop('user_id', None)
+        return redirect(url_for('login', error='User data not found, please log in again.'))
+
+    user_id = user['id']
+
+    device_res_counts = db.get_user_device_reservation_counts(user_id)
+    pie_colors_counts = ['#F15854', '#5DA5DA', '#DECF3F', '#4D4D4D']
+
+    pie_data_counts, gradient_counts, has_pie_counts = generate_pie_chart_segments(
+        device_res_counts,
+        pie_colors_counts,
+        label_key='device_name',
+        value_key='reservation_count')
+
+    device_res_durations = db.get_user_device_reservation_durations(user_id)
+
+    processed_device_res_durations = []
+    for item in device_res_durations:
+        processed_device_res_durations.append({
+            'device_name': item.get('device_name', 'Unknown Device'),
+            'total_duration_seconds': item.get('total_duration_seconds', 0),
+            'formatted_duration': format_duration_to_string(item.get('total_duration_seconds', 0))
+        })
+
+    pie_data_durations, gradient_durations, has_pie_durations = generate_pie_chart_segments(
+        processed_device_res_durations,
+        pie_colors_counts,
+        value_key='total_duration_seconds',
+        label_key='device_name',
+        preform_key='formatted_duration')
+
     return render_template('user_page.html',
                            username=username,
                            reservations=reservations,
                            devices=devices,
-                           last_reservations=last_reservations)
+                           last_reservations=last_reservations,
+                           pie_chart_data_counts=pie_data_counts,
+                           conic_gradient_style_counts=gradient_counts,
+                           has_reservations_for_pie_counts=has_pie_counts,
+                           pie_chart_data_durations=pie_data_durations,
+                           conic_gradient_style_durations=gradient_durations,
+                           has_reservations_for_pie_durations=has_pie_durations)
 
 @app.route('/device/<int:device_id>/add_comment', methods=['POST'])
 @login_required_with_csrf
