de18762c70f6e2a4db0d6440cc3cdde23dec7d00
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2025-06-23 23:37:01 +0300

    app.py: more restructure
    more restructuring, move util functions to slotkeeperutil
    
    Signed-off-by: Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import slotkeeperutil as su
 
 app = Flask(__name__)
 ITEMS_PER_PAGE = 20
+DATABASE = 'database.db'
 
 def login_required_with_csrf(f):
     '''wrapper to check login and for POST csrf status'''
@@ -25,35 +26,7 @@ def login_required_with_csrf(f):
         return f(*args, **kwargs)
     return decorated_function
 
-def fill_in_device_list(devices):
-    '''build device list for ui'''
-    device_data = []
-    user = db.get_user_by_username(session['username'])
-
-    for device in devices:
-        reservation = db.get_active_reservation_for_device(device['id'])
-        owned = reservation and reservation['user_id'] == user['id'] if reservation else False
-        desc = device['description'] or ''
-
-        # cut excessive long description to preview
-        lines = desc.splitlines()
-        preview = '\n'.join(lines[:3])[:250]
-        if len(desc) > 250 or desc.count('\n') >= 3:
-            preview += '\n...'
-
-        device_data.append(
-            {
-                'device': device,
-                'reservation': reservation,
-                'user_owned': owned,
-                'preview': preview
-            }
-        )
-    return device_data
-
 app.secret_key = config.secret_key  # Used to sign session cookies
-
-DATABASE = 'database.db'
 
 '''Instantiate the Database class'''
 db = Database(DATABASE)
@@ -75,7 +48,7 @@ def index():
         page = max(page, 1)
 
         devices = db.search_devices(query, user_id, only_mine, page=page, limit=ITEMS_PER_PAGE)
-        device_data = fill_in_device_list(devices['items'])
+        device_data = su.fill_in_device_list(session, db, devices['items'])
 
         csrf_token = su.generate_csrf_token(session)
         return render_template(
@@ -264,7 +237,7 @@ def reserve(device_id):
         page = max(page, 1)
 
         devices = db.search_devices(query, user_id, only_mine, page=page, limit=ITEMS_PER_PAGE)
-        device_data = fill_in_device_list(devices['items'])
+        device_data = su.fill_in_device_list(session, db, devices['items'])
 
         csrf_token = su.generate_csrf_token(session)
         return render_template(
@@ -337,8 +310,7 @@ def view_device(device_id):
     page = max(page, 1)
 
     devices = db.search_devices(query, user_id, only_mine, page=page, limit=ITEMS_PER_PAGE)
-
-    device_data = fill_in_device_list(devices['items'])
+    device_data = su.fill_in_device_list(session, db, devices['items'])
 
     error_message = request.args.get('error')
     csrf_token = su.generate_csrf_token(session)
@@ -357,70 +329,6 @@ def view_device(device_id):
         csrf_token=csrf_token,
         current_page=page,
         total_pages=int(devices['total'] / ITEMS_PER_PAGE))
-
-def generate_pie_chart_segments(device_list, colors, value_key='value', label_key='name',
-                                preform_key=None):
-    '''generate data to be shown in pie chart. top 3 machines + rest.'''
-    pie_data = []
-    gradient_parts = []
-    total_value = sum(item.get(value_key, 0) for item in device_list)
-    cur_angle = 0
-
-    if total_value > 0:
-        sorted_data = sorted(device_list, key=lambda x: x.get(value_key, 0), reverse=True)
-        top_n = 3
-        num_items_total = len(sorted_data)
-
-        pie_items = []
-        if num_items_total <= top_n:
-            pie_items.extend(sorted_data)
-        else:
-            pie_items.extend(sorted_data[:top_n])
-            others_value = sum(d.get(value_key, 0) for d in sorted_data[top_n:])
-            if others_value > 0:
-                other_item_data = {label_key: 'Other Devices', value_key: others_value}
-                if preform_key == 'formatted_duration':
-                    other_item_data[preform_key] = su.format_duration_to_string(others_value)
-                pie_items.append(other_item_data)
-
-        for i, input_list in enumerate(pie_items):
-            item_nvalue = input_list.get(value_key, 0)
-            percentage = 0
-            if total_value > 0:
-                percentage = (item_nvalue / total_value) * 100
-
-            # dict to template
-            item_dict = {
-                'name': input_list.get(label_key, 'Unknown'),
-                'value_numeric': item_nvalue,
-                'percentage': round(percentage, 1),
-                'color': colors[i % len(colors)]
-            }
-
-            if preform_key:
-                if preform_key in input_list:
-                    item_dict[preform_key] = input_list[preform_key]
-                else:
-                    item_dict[preform_key] = str(item_nvalue)
-
-            pie_data.append(item_dict)
-
-            start_percentage = cur_angle
-            is_last_segment = i == len(pie_items) - 1
-            if is_last_segment and cur_angle < 100:
-                cur_angle = 100.0
-            elif total_value > 0 :
-                cur_angle += percentage
-            cur_angle = min(max(cur_angle, 0), 100.0)
-            start_percentage = min(max(start_percentage,0), 100.0)
-            gradient_parts.append(
-                f'{colors[i % len(colors)]} {round(start_percentage, 2)}% {round(cur_angle, 2)}%'
-            )
-
-    conic_gradient_style = ''
-    if gradient_parts:
-        conic_gradient_style = f'background: conic-gradient({', '.join(gradient_parts)});'
-    return pie_data, conic_gradient_style, (total_value > 0)
 
 @app.route('/user')
 @login_required_with_csrf
@@ -442,7 +350,7 @@ def user_page():
     device_res_counts = db.get_user_device_reservation_counts(user_id)
     pie_colors_counts = ['#F15854', '#5DA5DA', '#DECF3F', '#4D4D4D']
 
-    pie_data_counts, gradient_counts, has_pie_counts = generate_pie_chart_segments(
+    pie_data_counts, gradient_counts, has_pie_counts = su.generate_pie_chart_segments(
         device_res_counts,
         pie_colors_counts,
         label_key='device_name',
@@ -458,7 +366,7 @@ def user_page():
             'formatted_duration': su.format_duration_to_string(item.get('total_duration_seconds', 0))
         })
 
-    pie_data_durations, gradient_durations, has_pie_durations = generate_pie_chart_segments(
+    pie_data_durations, gradient_durations, has_pie_durations = su.generate_pie_chart_segments(
         processed_device_res_durations,
         pie_colors_counts,
         value_key='total_duration_seconds',
--- slotkeeperutil.py
+++ slotkeeperutil.py
@@ -37,3 +37,93 @@ def generate_csrf_token(session):
     token = str(uuid.uuid4())
     session['csrf_token'] = token
     return token
+
+def fill_in_device_list(session, db, devices):
+    '''build device list for ui'''
+    device_data = []
+    user = db.get_user_by_username(session['username'])
+
+    for device in devices:
+        reservation = db.get_active_reservation_for_device(device['id'])
+        owned = reservation and reservation['user_id'] == user['id'] if reservation else False
+        desc = device['description'] or ''
+
+        # cut excessive long description to preview
+        lines = desc.splitlines()
+        preview = '\n'.join(lines[:3])[:250]
+        if len(desc) > 250 or desc.count('\n') >= 3:
+            preview += '\n...'
+
+        device_data.append(
+            {
+                'device': device,
+                'reservation': reservation,
+                'user_owned': owned,
+                'preview': preview
+            }
+        )
+    return device_data
+
+def generate_pie_chart_segments(device_list, colors, value_key='value', label_key='name',
+                                preform_key=None):
+    '''generate data to be shown in pie chart. top 3 machines + rest.'''
+    pie_data = []
+    gradient_parts = []
+    total_value = sum(item.get(value_key, 0) for item in device_list)
+    cur_angle = 0
+
+    if total_value > 0:
+        sorted_data = sorted(device_list, key=lambda x: x.get(value_key, 0), reverse=True)
+        top_n = 3
+        num_items_total = len(sorted_data)
+
+        pie_items = []
+        if num_items_total <= top_n:
+            pie_items.extend(sorted_data)
+        else:
+            pie_items.extend(sorted_data[:top_n])
+            others_value = sum(d.get(value_key, 0) for d in sorted_data[top_n:])
+            if others_value > 0:
+                other_item_data = {label_key: 'Other Devices', value_key: others_value}
+                if preform_key == 'formatted_duration':
+                    other_item_data[preform_key] = format_duration_to_string(others_value)
+                pie_items.append(other_item_data)
+
+        for i, input_list in enumerate(pie_items):
+            item_nvalue = input_list.get(value_key, 0)
+            percentage = 0
+            if total_value > 0:
+                percentage = (item_nvalue / total_value) * 100
+
+            # dict to template
+            item_dict = {
+                'name': input_list.get(label_key, 'Unknown'),
+                'value_numeric': item_nvalue,
+                'percentage': round(percentage, 1),
+                'color': colors[i % len(colors)]
+            }
+
+            if preform_key:
+                if preform_key in input_list:
+                    item_dict[preform_key] = input_list[preform_key]
+                else:
+                    item_dict[preform_key] = str(item_nvalue)
+
+            pie_data.append(item_dict)
+
+            start_percentage = cur_angle
+            is_last_segment = i == len(pie_items) - 1
+            if is_last_segment and cur_angle < 100:
+                cur_angle = 100.0
+            elif total_value > 0 :
+                cur_angle += percentage
+            cur_angle = min(max(cur_angle, 0), 100.0)
+            start_percentage = min(max(start_percentage,0), 100.0)
+            gradient_parts.append(
+                f'{colors[i % len(colors)]} {round(start_percentage, 2)}% {round(cur_angle, 2)}%'
+            )
+
+    conic_gradient_style = ''
+    if gradient_parts:
+        conic_gradient_style = f'background: conic-gradient({', '.join(gradient_parts)});'
+    return pie_data, conic_gradient_style, (total_value > 0)
